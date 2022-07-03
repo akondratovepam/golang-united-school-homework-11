5b9e7e79fbef5cff5b763e5c8cc1f65cc7aad4a1
Andrey <andrey_kondratov@epam.com>
2022-07-03 18:31:57 +0300

    Added homework

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,29 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var ids = make(chan int64)
+	var i int64
+	var wg sync.WaitGroup
+	var mx sync.Mutex
+	for i = 0; i < pool; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for id := range ids {
+				user := getOne(id)
+
+				mx.Lock()
+				res = append(res, user)
+				mx.Unlock()
+			}
+		}()
+	}
+
+	for i = 0; i < n; i++ {
+		ids <- i
+	}
+
+	close(ids)
+	wg.Wait()
+	return
 }
